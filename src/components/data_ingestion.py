a5f287737f56fb2ea4bbb295c65172386b7b1f0a
yajusha <yajhusharavi@gmail.com>
2023-05-02 17:05:25 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -50,8 +50,8 @@ class DataIngestion:
         
 ##Run Data
 
-if __name__=='__main__':
+''' if __name__=='__main__':
     obj = DataIngestion()
     train_data_path, test_data_path = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initaite_data_transformation(train_data_path, test_data_path)
+    data_transformation.initaite_data_transformation(train_data_path, test_data_path) '''
